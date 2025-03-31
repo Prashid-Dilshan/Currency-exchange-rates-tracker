142ad9d21806bbf4719c985a24e1324c4078fd97
Prashid710 <prashiddilshan0710@gmail.com>
2025-03-31 14:36:55 +0530

    edit

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Mainpage from './Page/Mainpage';
 
+
 function App() {
   return (
     <div>
--- client/src/Page/Mainpage.js
+++ client/src/Page/Mainpage.js
@@ -1,12 +1,93 @@
-import React from 'react'
+import React, { useState } from "react";
+import "../App.css";
 
 function Mainpage() {
+  //State for the form fill
+  const [date, setDate] = useState(null);
+  const [sourceCurrency, setSourceCurrency] = useState("");
+  const [targetCurrency, setTargetCurrency] = useState("");
+  const [amountInSourceCurrency, setAmountInSourceCurrency] = useState();
+  const [amountInTargetCurrency, setAmountInTargetCurrency] = useState(0);
+
+
+  //handle submit method
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(
+      date,
+      setSourceCurrency,
+      setTargetCurrency,
+      amountInSourceCurrency
+    );
+  };
+
   return (
     <div>
-      <h1>Corrency rate checker</h1>
-      <p>siufh aiuh iwuhiwuitwbitjh iwuhtoiugwreguht ewiuhgtwiuerhg tihfjbdsjkf ahj fk afiujhiwsjkhf iwuhrwjehbr kwejrwkjeriwkue griwjegriwuhgiru wiuerwiuehgrwikejhr wieuryiuweiurwert w w rtwertwert</p>
+      <h1 className="bg-inherit"> Corrency rate checker</h1>
+      <p>
+        siufh aiuh iwuhiwuitwbitjh iwuhtoiugwreguht ewiuhgtwiuerhg tihfjbdsjkf
+        ahj fk afiujhiwsjkhf iwuhrwjehbr kwejrwkjeriwkue griwjegriwuhgiru
+        wiuerwiuehgrwikejhr wieuryiuweiurwert w w rtwertwert
+      </p>
+
+      <div>
+        <section>
+          <form onSubmit={handleSubmit}>
+
+            <div>
+              <label htmlFor={date}>Date : </label>
+              <input onChange={(e) => setDate(e.target.value)} type="date" id={date} name={date}></input>
+            </div>
+
+
+            <div>
+              <label  htmlFor={sourceCurrency}>Source currency : </label>
+              <select
+              onChange={(e) => setSourceCurrency(e.target.value)}
+                name={sourceCurrency}
+                id={sourceCurrency}
+                value={sourceCurrency}
+              >
+                <option value=""> select the source currency </option>
+              </select>
+            </div>
+
+
+            <div>
+              <label htmlFor={targetCurrency}> select Target currency : </label>
+              <select
+              onChange={(e) => setTargetCurrency(e.target.value)}
+                name={targetCurrency}
+                id={targetCurrency}
+                value={targetCurrency}
+              >
+                <option value=""> select Target currency </option>
+              </select>
+            </div>
+
+
+            <div>
+              <label htmlFor={amountInSourceCurrency}>
+                Amount is source currency :{" "}
+              </label>
+              <input
+              onChange={(e) => setAmountInSourceCurrency(e.target.value)}
+                type="text"
+                id={amountInSourceCurrency}
+                name={amountInSourceCurrency}
+                value={amountInSourceCurrency}
+              ></input>
+            </div>
+
+
+            <button>Get Value</button>
+
+            
+          </form>
+        </section>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Mainpage
+export default Mainpage;
