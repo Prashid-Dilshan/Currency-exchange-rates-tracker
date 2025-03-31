07e5498b928156343100e4ba5d1e591c399e5ce4
Prashid710 <prashiddilshan0710@gmail.com>
2025-03-31 12:16:29 +0530

    edit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Mainpage from './Page/Mainpage';
 
 function App() {
   return (
     <div>
-dfgsf 
+<Mainpage></Mainpage>
     </div>
   );
 }
---
+++ client/src/Page/Mainpage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Mainpage() {
+  return (
+    <div>
+      <h1>Corrency rate checker</h1>
+      <p>siufh aiuh iwuhiwuitwbitjh iwuhtoiugwreguht ewiuhgtwiuerhg tihfjbdsjkf ahj fk afiujhiwsjkhf iwuhrwjehbr kwejrwkjeriwkue griwjegriwuhgiru wiuerwiuehgrwikejhr wieuryiuweiurwert w w rtwertwert</p>
+    </div>
+  )
+}
+
+export default Mainpage
